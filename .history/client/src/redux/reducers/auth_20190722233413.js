19c1fab119936b38dc791aa8f9f24ae02e03b5e6
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-07-24 19:37:04 +0800

    auth

---
+++ .history/client/src/redux/reducers/auth_20190722233413.js
@@ -0,0 +1,47 @@
+
+ 
+ const initialState = {
+   token: localStorage.getItem('token'),
+   isAuthenticated: null,
+   loading: true,
+   user: null
+ };
+ 
+ export default function(state = initialState, action) {
+   const { type, payload } = action;
+
+ console.log(initialState.token)
+   switch (type) {
+     case 'USER_LOADED':
+       return {
+         ...state,
+         isAuthenticated: true,
+         loading: false,
+         user: payload
+       };
+     case 'REGISTER_SUCCESS':
+       localStorage.setItem('token', payload.token);
+       return {
+         ...state,
+         ...payload,
+         isAuthenticated: true,
+         loading: false
+       };
+     case 'REGISTER_FAIL':
+     case 'AUTH_ERROR':
+
+       localStorage.removeItem('token');
+       return {
+         ...state,
+         token: null,
+         isAuthenticated: false,
+         loading: false
+       };
+     default:
+       return state;
+   }
+ }
+ 
+
+
+
---
+++ .history/routes/api/authRoutes/getMyProfileAuth_20190723122816.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const auth = require('../../middleware/auth');
+const getMyProfile = express.Router();
+const User = require('../../models/User')
+const { check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+//Return my profile
+
+authRouter.get('/', auth, async (req, res) => {
+      try {
+             const user = await User.findById(req.user.id);
+            res.json(user)
+      } catch (error) {
+         console.log(error.message);
+         res.status(500).send('server error')
+      }
+});
+
+
+
+module.exports = authRouter;
+
