9cbcec756a28c8b0be295a746107f891223a52e7
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-03 07:00:58 +0800

    Refactoring

--- .history/app_20190803070027.js
+++ .history/app_20190803070027.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 
 
 //Connect to DB
- mongodbConnect()
+ //mongodbConnect()
 
 // mongoose.connect('mongodb://localhost/My-Budget-App', {
 //         useNewUrlParser: true,
